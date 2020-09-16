c7843e63d37dd1e7ebffaaf73706d2f30fb726f9
Huseyn <70753221+0Rogue0@users.noreply.github.com>
2020-09-16 23:08:57 +0300

    Update Ball.java

--- Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Ball.java
+++ Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Ball.java
@@ -1,4 +1,28 @@
 package Lab1;
+import java.lang.*;
+public class Book {
+    private String author;
+    private int year;
 
-public class Ball {
+    public Book(String a, int y){
+        author = a;
+        year = y;
+    }
+
+    public Book(String a){
+        author = a;
+        year = 1966;
+    }
+
+    public Book() {
+        author = "Tolstoy";
+        year = 1867;
+    }
+
+    public void setYear(int year) {this.year = year;}
+    public void setAuthor(String author) {this.author = author;}
+    public String getAuthor() {return author;}
+    public int getYear() {return year;}
+    public String toString() {return "Book`s author " + author + ", and its date of issue is — " + year;}
+
 }
