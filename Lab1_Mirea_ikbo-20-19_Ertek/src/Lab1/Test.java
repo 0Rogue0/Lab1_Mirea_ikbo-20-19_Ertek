fe81654f4a5c7f2f1220c3e3771dc3b1fa29e391
Huseyn <70753221+0Rogue0@users.noreply.github.com>
2020-09-16 23:09:48 +0300

    Update Test.java

--- Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Test.java
+++ Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Test.java
@@ -1,4 +1,25 @@
 package Lab1;
-
+import java.lang.*;
 public class Test {
+    public static void main(String[] args) {
+        Dog d1 = new Dog("Booba", 2);
+        Dog d2 = new Dog();
+        Dog d3 = new Dog("Pipyao",4);
+        System.out.println(d1.toString());
+        System.out.println(d2.toString());
+        System.out.println(d3.toString());
+        Ball q1 = new Ball("Red", 10);
+        Ball q2 = new Ball("Yellow", 8);
+        Ball q3 = new Ball("Black");
+        System.out.println(q1.toString());
+        System.out.println(q2.toString());
+        System.out.println(q3.toString());
+        Book b1 = new Book("Pushkin", 1833);
+        Book b2 = new Book("Bulgakov");
+        Book b3 = new Book();
+        System.out.println(b1.toString());
+        System.out.println(b2.toString());
+        System.out.println(b3.toString());
+    }
+
 }
