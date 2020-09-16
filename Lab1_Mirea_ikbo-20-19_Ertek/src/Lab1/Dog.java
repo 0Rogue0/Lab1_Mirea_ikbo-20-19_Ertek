62209c35e049ba009a0003523176daabcee22845
Huseyn <70753221+0Rogue0@users.noreply.github.com>
2020-09-16 23:09:27 +0300

    Update Dog.java

--- Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Dog.java
+++ Lab1_Mirea_ikbo-20-19_Ertek/src/Lab1/Dog.java
@@ -1,4 +1,28 @@
 package Lab1;
-
+import java.lang.*;
 public class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String n, int a){
+        name = n;
+        age = a;
+    }
+
+    public Dog(){
+        name = "Ovsyanka";
+        age = 2;
+    }
+
+    public Dog(String n){
+        name = n;
+        age = 0;
+    }
+    public void setName(String name) {this.name = name;}
+    public void setAge(int age) {this.age = age;}
+    public int getAge() {return age;}
+    public String getName(String name){return name;}
+    public String toString() {
+        return name + "`s age in human years is " + age * 7 + " years";
+    }
 }
